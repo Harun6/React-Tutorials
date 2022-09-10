3f9c28de0a2f2cc82fdc0c5924725fdc2352c4b6
Harun Akgul <46harunakgul@gmail.com>
2022-09-10 15:28:18 +0300

    Practice

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1 className="naber">Hello React</h1>
+      <label htmlFor =""></label> 
     </div>
   );
 }
