6325ff659bc0f9fa11c860a19bb18d7696c8e45d
Harun Akgul <46harunakgul@gmail.com>
2022-09-12 20:46:01 +0300

    More Practice

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,10 +2,21 @@
 import './App.css';
 
 function App() {
+  const test = 34;
+  const isAuth = true;
   return (
     <div className="App">
       <h1 className="naber">Hello React</h1>
-      <label htmlFor =""></label> 
+      <label htmlFor =""></label>
+      <h4>{test}</h4>              {/*JavaScript is written in square brackets.*/}
+      <h4>{1 + 1}</h4>
+      <h4>{"harun".toLocaleUpperCase()}</h4>
+      <div>
+        {
+          isAuth ? <p> User Registered</p>
+          : null
+        }
+      </div>
     </div>
   );
 }
