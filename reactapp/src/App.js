5b1f0715676a929fcd808c01a1a9a66e5ecd8074
Harun Akgul <46harunakgul@gmail.com>
2022-09-12 21:48:23 +0300

    Add User.js and Component

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,22 +1,38 @@
 
+import User from './Component/User';
 import './App.css';
 
 function App() {
-  const test = 34;
-  const isAuth = true;
+ // const test = 34;
+ // const isAuth = true;
   return (
     <div className="App">
-      <h1 className="naber">Hello React</h1>
+      {/*<h1 className="naber">Hello React</h1>
       <label htmlFor =""></label>
       <h4>{test}</h4>              {/*JavaScript is written in square brackets.*/}
-      <h4>{1 + 1}</h4>
+      {/*<h4>{1 + 1}</h4>
       <h4>{"harun".toLocaleUpperCase()}</h4>
       <div>
         {
           isAuth ? <p> User Registered</p>
           : null
         }
-      </div>
+      </div>*/}
+      {/* App Component*/}
+      <h4>App Component</h4>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
+
+      <User/>
+      <User/>
+
+      <User/>
+      <User/>
+      <User/>
+
+      <User/>
     </div>
   );
 }
