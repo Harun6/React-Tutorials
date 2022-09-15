6841e861d4fa66ed0f2057f59bbeb32fadac16c5
Harun Akgul <46harunakgul@gmail.com>
2022-09-15 22:19:32 +0300

    More Practice.

--- reactapp/src/Component/User.js
+++ reactapp/src/Component/User.js
@@ -3,23 +3,31 @@ import PropTypes from 'prop-types';
 
 
  class User extends Component {
+
+  state = {
+    isVisible : false
+  }
  
-  constructor(props){
-    super(props);
-    
-    this.state = {
-      isVisible : false
-    }
+  /*constructor(props){
+    super(props)
+    this.onClickEvent = this.onClickEvent.bind(this);
+  }*/
+
+  onClickEvent = (event) => {
+    this.setState ( {
+      isVisible : !this.state.isVisible
+    })
   }
   
   render() {
     // Destructing
     const {name,department,salary} = this.props;
+    const {isVisible} = this.state;
     return (
       <div className = 'col-md-8 mb-4'>
         <div className = 'card'>
           <div className = 'card-header d-flex justify-content-between'>
-            <h4 className = 'd-inline'>{name}</h4>
+            <h4 className = 'd-inline' onClick={this.onClickEvent}>{name}</h4>
             <i className = 'far fa-trash-alt' style = {{cursor : "pointer"}}></i>
           </div>
           {
@@ -31,6 +39,7 @@ import PropTypes from 'prop-types';
           
         </div>        
       </div>
+      
     )
   }
 }
