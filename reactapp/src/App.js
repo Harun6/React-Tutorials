fd7336726c47ed23df32ee279991fea33682c026
Harun Akgul <46harunakgul@gmail.com>
2022-09-17 00:09:17 +0300

    Component Communication

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,43 +1,41 @@
 
-import Navbar from "./Component/Navbar";
-import User from "./Component/User";
-import './App.css';
+import React, { Component } from 'react'
 
+import Navbar from './Component/Navbar'
+import Users from './Component/Users'
+import './App.css'
 
-function App() {
- // const test = 34;
- // const isAuth = true;
-  return (
-    <div className="container">
-      {/*<h1 className="naber">Hello React</h1>
-      <label htmlFor =""></label>
-      <h4>{test}</h4>              {/*JavaScript is written in square brackets.*/}
-      {/*<h4>{1 + 1}</h4>
-      <h4>{"harun".toLocaleUpperCase()}</h4>
-      <div>
-        {
-          isAuth ? <p> User Registered</p>
-          : null
-        }
-      </div>*/}
-      {/* App Component
-      <h4>App Component</h4>
-      <User/>*/}
-      <Navbar title ="User App "/>
-      <hr/>   
-      <User
-      name = "Harun Akgül"
-      department = "Bilişim"
-      salary = "5000"
-      />   
-
-      <User
-      name = "Mehmet Yıldız"
-      department = "Bilişim"
-      salary = "6000"
-      /> 
+export default class App extends Component {
+  state = {
+    users : [
+      {
+        id : 1,
+        name : 'Harun Akgül',
+        salary : 6000,
+        department : 'Bilişim' 
+      },
+      {
+        id : 1,
+        name : 'Mehmet Yıldız',
+        salary : 5000,
+        department : 'Bilişim' 
+      },
+      {
+        id : 1,
+        name : 'Emre Yürük',
+        salary : 4000,
+        department : 'İnsan Kaynakları' 
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="container">
+        <Navbar title = "User App"/>
+        <hr/>   
+        <Users users = {this.state.users}/>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
